d70562d36407b58187b99e2d17b58e5d96e9ccaa
2unty <101151564+2unty@users.noreply.github.com>
2025-10-01 21:57:40 -0400

    Update Grid.h
    Struct verision

--- Grid.h
+++ Grid.h
@@ -1,65 +1,71 @@
 #pragma once
 #include <string>
-void cdraw(int arg, int x, int y) {
-    int x1 = arg + 1;
-    int split = x1 / 2;
-    bool yevennum = true;
-    bool found = false;
-    int xnum;
-    int ynum = arg / 2;
-    for (int i = 0; i < x1 * 2; i++) {
-        xnum = -(arg / 2);
-        for (int k = 0; k < x1; k++) {
-            if (i == split * 2) {
-                printf("* * ");
-            }
-            else if (i == split * 2 + 1) {
-                printf("* * ");
-            }
-            else if (i == split * 2 + 2) {
-                if (xnum < 0) {
-                    if (xnum < -9) {
-                        printf("%d ", xnum);
-                    }
-                    else {
-                        printf(" %d ", xnum);
-                    }
-                }
-                else if (xnum == 0) {
+class DrawGrid {
+public:
+    int Gridsize;
+    int X;
+    int Y;
+    void cdraw() {
+        int x1 = Gridsize + 1;
+        int split = x1 / 2;
+        bool yevennum = true;
+        bool found = false;
+        int xnum;
+        int ynum = Gridsize / 2;
+        for (int i = 0; i < x1 * 2; i++) {
+            xnum = -(Gridsize / 2);
+            for (int k = 0; k < x1; k++) {
+                if (i == split * 2) {
                     printf("* * ");
                 }
-                else if (xnum > 9) {
-                    printf(" %d ", xnum);
+                else if (i == split * 2 + 1) {
+                    printf("* * ");
+                }
+                else if (i == split * 2 + 2) {
+                    if (xnum < 0) {
+                        if (xnum < -9) {
+                            printf("%d ", xnum);
+                        }
+                        else {
+                            printf(" %d ", xnum);
+                        }
+                    }
+                    else if (xnum == 0) {
+                        printf("* * ");
+                    }
+                    else if (xnum > 9) {
+                        printf(" %d ", xnum);
+                    }
+                    else {
+                        printf("  %d ", xnum);
+                    }
+                }
+                else if (k == split) {
+                    printf("* * ");
+                }
+                else if (k == split + 1) {
+                    if (yevennum) {
+                        if (ynum == 0) {
+                            ynum = -1;
+                        }
+                        printf("%d", ynum);
+                        ynum--;
+                        yevennum = false;
+                    }
+                    else if (not yevennum) {
+                        yevennum = true;
+                    }
+                }
+                else if (xnum == X + 1 and ynum == Y - 1 and not found) {
+                    printf(" +  ");
+                    found = true;
                 }
                 else {
-                    printf("  %d ", xnum);
+                    printf("    ");
                 }
+                xnum++;
             }
-            else if (k == split) {
-                printf("* * ");
-            }
-            else if (k == split + 1) {
-                if (yevennum) {
-                    if (ynum == 0) {
-                        ynum = -1;
-                    }
-                    printf("%d", ynum);
-                    ynum--;
-                    yevennum = false;
-                }
-                else if (not yevennum) {
-                    yevennum = true;
-                }
-            }
-            else if (xnum == x + 1 and ynum == y - 1 and not found) {
-                printf(" +  ");
-                found = true;
-            }
-            else {
-                printf("    ");
-            }
-            xnum++;
+            printf("\n");
         }
-        printf("\n");
     }
-}
+};
